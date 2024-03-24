655413443c85e12818c053ad12915a1381216440
rubygitflow <nrubygitflow@gmail.com>
2024-03-24 19:28:10 +0600

    add Two Sum II

--- README.md
+++ README.md
@@ -2,11 +2,12 @@
 Solving algorithmic problems on leetcode.com
 
 - [1. Two Sum](https://leetcode.com/problems/two-sum/) >>> [two_sum.exs](https://github.com/rubygitflow/leetcode_elixir/blob/master/two_sum.exs)
+- [167. Two Sum II - Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) >>> [two_sum.exs](https://github.com/rubygitflow/leetcode_elixir/blob/master/two_sum.exs)
 - [870. Advantage Shuffle](https://leetcode.com/problems/advantage-shuffle/) >>> [advantage_shuffle.exs](https://github.com/rubygitflow/leetcode_elixir/blob/master/advantage_shuffle.exs)
 - [1723. Find Minimum Time to Finish All Jobs](https://leetcode.com/problems/find-minimum-time-to-finish-all-jobs/) >>> [find_minimum_time_to_finish_all_jobs.exs](https://github.com/rubygitflow/leetcode_elixir/blob/master/find_minimum_time_to_finish_all_jobs.exs)
 - [1736. Latest Time by Replacing Hidden Digits](https://leetcode.com/problems/latest-time-by-replacing-hidden-digits/) >>> [latest_time_by_replacing_hidden_digits.exs](https://github.com/rubygitflow/leetcode_elixir/blob/master/latest_time_by_replacing_hidden_digits.exs)
 
-**Tasks: 4  >>> Files: 4**
+**Tasks: 5  >>> Files: 4**
 
 ## Install
 ```shell
--- two_sum.exs
+++ two_sum.exs
@@ -1,6 +1,10 @@
 # https://leetcode.com/problems/two-sum/description/
 # 1. Two Sum
 
+#######################
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+# 167. Two Sum II - Input Array Is Sorted
+
 defmodule Solution do
   @spec two_sum(nums :: [integer], target :: integer) :: [integer]
   def two_sum(nums, target) do
@@ -16,8 +20,37 @@ defmodule Solution do
       find_two_sum(tail, target, position+1, Map.put_new(seen, head, position))
     end
   end
+
+  @spec two_sum_sorted(numbers :: [integer], target :: integer) :: [integer]
+  def two_sum_sorted(numbers, target) do
+    [head | tail] = numbers
+    fetch_two_sum_sorted(head, 0, tail, target)
+  end
+  def fetch_two_sum_sorted(_value, _position, nums, _target) when length(nums) < 1, do: []
+  def fetch_two_sum_sorted(value, position, nums, target) do
+    res = target - value
+    {out_r, out_v} = Enum.reduce_while(nums, {0, '*'}, fn num, acc ->
+      {pos, val} = acc
+      if num == res do
+        {:halt, {pos+1, num}}
+      else
+        if num < res do
+          {:cont, {pos+1, val}}
+        else
+          {:halt, {0, val}}
+        end
+      end
+    end)
+    if out_v == '*'  do
+      [head | tail] = nums
+      fetch_two_sum_sorted(head, position+1, tail, target)
+    else
+      [position+1, out_r + position + 1]
+    end
+  end
 end
 
+IO.puts('Two Sum')
 [2,7,11,15] |> Solution.two_sum(9) |> IO.inspect()
 # Output: [0,1]
 [3,2,4] |> Solution.two_sum(6) |> IO.inspect()
@@ -26,3 +59,15 @@ end
 # Output: [0,1]
 [3,3] |> Solution.two_sum(5) |> IO.inspect()
 # Output: []
+
+IO.puts('Two Sum II - Input Array Is Sorted')
+[2,7,11,15] |> Solution.two_sum_sorted(9) |> IO.inspect()
+# Output: [1,2]
+[2,3,4] |> Solution.two_sum_sorted(6) |> IO.inspect()
+# Output: [1,3]
+[-1,0] |> Solution.two_sum_sorted(-1) |> IO.inspect()
+# Output: [1,2]
+[3,3] |> Solution.two_sum_sorted(5) |> IO.inspect()
+# Output: []
+[2,7,11,15,15,16,17,18,19,20,21,22,23,24,25] |> Solution.two_sum_sorted(46) |> IO.inspect()
+# Output: [11,15]
