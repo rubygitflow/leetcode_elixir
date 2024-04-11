e5aea590360e946c09a32d44c0d8e4238b237bc0
rubygitflow <nrubygitflow@gmail.com>
2024-04-11 21:47:10 +0600

    add Search a 2D Matrix

--- binary_search.exs
+++ binary_search.exs
@@ -18,6 +18,19 @@ defmodule Solution do
     end
   end
   defp binary_search(tuple, _left, _right, _target) when is_tuple(tuple), do: -1
+
+  @spec search_matrix(matrix :: [[integer]], target :: integer) :: boolean
+  def search_matrix(matrix, target) do
+    vector = List.to_tuple(matrix |> Enum.reduce_while([], fn x, acc ->
+      if hd(x) <= target do
+        {:cont, x}
+      else
+        {:halt, acc}
+      end
+    end))
+    output = binary_search(vector, 0, tuple_size(vector) - 1, target)
+    if output < 0, do: false, else: true
+  end
 end
 
 IO.inspect("Binary Search")
@@ -26,3 +39,12 @@ Solution.search([-1,0,3,5,9,12], 9) |> IO.inspect()
 Solution.search([-1,0,3,5,9,12], 2) |> IO.inspect()
 # Output: -1
 
+IO.inspect("Search a 2D Matrix")
+Solution.search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], -2) |> IO.inspect()
+# Output: false
+Solution.search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13) |> IO.inspect()
+# Output: false
+Solution.search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 23) |> IO.inspect()
+# Output: true
+Solution.search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 34) |> IO.inspect()
+# Output: true
