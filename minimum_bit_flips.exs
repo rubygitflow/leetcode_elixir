9e903faa514016aed5909ce04e0cf59d6fd6f18f
rubygitflow <nrubygitflow@gmail.com>
2024-06-09 04:38:05 +0600

    add a test case

--- minimum_bit_flips.exs
+++ minimum_bit_flips.exs
@@ -12,6 +12,15 @@ defmodule Solution do
     # and count them
     |> Enum.sum()
   end
+
+  def min_bit_flips_ii(start, goal) do
+    while_not_zero(0, Bitwise.bxor(start, goal))
+  end
+  defp while_not_zero(out, tmp) when tmp > 0 do
+    while_not_zero(out + Bitwise.band(tmp, 1), Bitwise.bsr(tmp, 1))
+  end
+  # Important! Call the Return after Calling "when tmp > 0"
+  defp while_not_zero(out, _), do: out
 end
 
 IO.inspect(Solution.min_bit_flips(10, 7))
@@ -19,4 +28,11 @@ IO.inspect(Solution.min_bit_flips(10, 7))
 IO.inspect(Solution.min_bit_flips(3, 4))
 # Output: 3
 IO.inspect(Solution.min_bit_flips(10**9, 1))
+# Output: 14
+
+IO.inspect(Solution.min_bit_flips_ii(10, 7))
+# Output: 3
+IO.inspect(Solution.min_bit_flips_ii(3, 4))
+# Output: 3
+IO.inspect(Solution.min_bit_flips_ii(10**9, 1))
 # Output: 14
