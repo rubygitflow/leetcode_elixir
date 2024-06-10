428bf424887cc089b8f363d42e2e39bcbd2dbe62
rubygitflow <nrubygitflow@gmail.com>
2024-06-10 16:46:37 +0600

    add a test case

--- find_triangular_sum.exs
+++ find_triangular_sum.exs
@@ -15,10 +15,25 @@ defmodule Solution do
       rem(n1 + n2, 10)
     end)
   end
+
+  def triangular_sum_ii(nums) do
+    len = Enum.count(nums) - 1
+    nums
+    |> Enum.reduce({0, 1, 0}, fn num, {i, mCk, res} ->
+         {i + 1, div(mCk*(len - i), (i + 1)), rem(res + num*mCk, 10)}
+       end)
+    |> elem(2)
+  end
 end
 
+IO.inspect("Find Triangular Sum of an Array (recursive)")
 IO.inspect(Solution.triangular_sum([1,2,3,4,5]))
 # Output: 8
 IO.inspect(Solution.triangular_sum([5]))
 # Output: 5
 
+IO.inspect("Find Triangular Sum of an Array (Pascal Triangle)")
+IO.inspect(Solution.triangular_sum_ii([1,2,3,4,5]))
+# Output: 8
+IO.inspect(Solution.triangular_sum_ii([5]))
+# Output: 5
