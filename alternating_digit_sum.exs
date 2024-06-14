a5dad8e405910f3ac8eb03c530449df3c5a97c4a
rubygitflow <nrubygitflow@gmail.com>
2024-06-14 08:23:00 +0600

    add Add Digits

--- alternating_digit_sum.exs
+++ alternating_digit_sum.exs
@@ -1,9 +1,15 @@
 # https://leetcode.com/problems/alternating-digit-sum/description/
 # 2544. Alternating Digit Sum
 
+#############################
+# https://leetcode.com/problems/add-digits/description/
+# 258. Add Digits
+
+
 import Integer
 
 defmodule Solution do
+  # Alternating Digit Sum
   @spec alternate_digit_sum(n :: integer) :: integer
   def alternate_digit_sum(n), do: do_alternate_digit_sum(n, 1, 0, 0)
 
@@ -17,8 +23,20 @@ defmodule Solution do
   end
   defp do_alternate_digit_sum(_, _, digits_num, acc) when is_even(digits_num), do: -acc
   defp do_alternate_digit_sum(_, _, _digits_num, acc), do: acc
+
+  # Add Digits
+  @spec add_digits(num :: integer) :: integer
+  def add_digits(num) when num >= 10 do
+    add_digits(
+      num
+      |> Integer.digits()
+      |> Enum.sum
+    )
+  end
+  def add_digits(num), do: num
 end
 
+IO.inspect("Alternating Digit Sum")
 IO.inspect(Solution.alternate_digit_sum(521))
 # Output: 4
 IO.inspect(Solution.alternate_digit_sum(111))
@@ -29,3 +47,13 @@ IO.inspect(Solution.alternate_digit_sum(885996))
 # Output: -1
 IO.inspect(Solution.alternate_digit_sum(886995))
 # Output: 1
+
+IO.inspect("Add Digits (recursion)")
+IO.inspect(Solution.add_digits(38))
+# Output: 2
+IO.inspect(Solution.add_digits(0))
+# Output: 0
+IO.inspect(Solution.add_digits(886995))
+# 8+8+6+9+9+5=45
+# 4+5=9
+# Output: 9
