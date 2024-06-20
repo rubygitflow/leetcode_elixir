74bb93306604d5c76325663ab2d7c9605e2e4369
rubygitflow <nrubygitflow@gmail.com>
2024-06-20 06:48:25 +0600

    add a test case

--- common_prefix.exs
+++ common_prefix.exs
@@ -14,6 +14,20 @@ defmodule Solution do
     |> Enum.filter(&match?([_], &1)) # Array of chars has only one character
     |> IO.iodata_to_binary()
   end
+
+  @spec longest_common_prefix_ii(strs :: [String.t()]) :: String.t()
+  def longest_common_prefix_ii(strs) when length(strs) < 2, do: ""
+  def longest_common_prefix_ii(strs) do
+    Enum.reduce(strs, &take_while_eq/2)
+  end
+
+  defp take_while_eq(str1, str2, acc \\ "")
+  defp take_while_eq(<<l1::binary-size(1), rest1::bitstring()>>,
+                     <<l2::binary-size(1), rest2::bitstring()>>,
+                     acc) when l1 == l2 do
+    take_while_eq(rest1, rest2, acc <> l1)
+  end
+  defp take_while_eq(_,_, acc), do: acc
 end
 
 
@@ -25,4 +39,13 @@ IO.inspect(Solution.longest_common_prefix(["dog","racecar","car"]))
 IO.inspect(Solution.longest_common_prefix(["flow","flow","flow"]))
 # Output: "flow"
 IO.inspect(Solution.longest_common_prefix(["dog"]))
+# Output: ""
+
+IO.inspect(Solution.longest_common_prefix_ii(["flower","flow","flight"]))
+# Output: "fl"
+IO.inspect(Solution.longest_common_prefix_ii(["dog","racecar","car"]))
+# Output: ""
+IO.inspect(Solution.longest_common_prefix_ii(["flow","flow","flow"]))
+# Output: "flow"
+IO.inspect(Solution.longest_common_prefix_ii(["dog"]))
 # Output: ""
