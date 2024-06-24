f3463babc67f1949adba258e62da10a5eb85d83c
rubygitflow <nrubygitflow@gmail.com>
2024-06-25 05:07:06 +0600

    add Single Number II

--- single_number.exs
+++ single_number.exs
@@ -1,10 +1,20 @@
 # https://leetcode.com/problems/single-number/
 # 136. Single Number
 
+#######################
+# https://leetcode.com/problems/single-number-ii/description/
+# 137. Single Number II
+
+
 defmodule Solution do
   @spec single_number(nums :: [integer]) :: integer
   def single_number(nums) do
     Enum.reduce(nums, &Bitwise.bxor/2)
+  end
+
+  @spec single_number_ii(nums :: [integer]) :: integer
+  def single_number_ii(nums) do
+    hd(for {a, b} <- Enum.frequencies(nums), b == 1, do: a)
   end
 end
 
@@ -18,3 +28,9 @@ IO.inspect(Solution.single_number([4,1,2,1,2]))
 # Output: 4
 IO.inspect(Solution.single_number([1]))
 # Output: 1
+
+IO.inspect("Single Number II")
+IO.inspect(Solution.single_number_ii([2,2,3,2]))
+# Output: 3
+IO.inspect(Solution.single_number_ii([0,1,0,1,0,1,99]))
+# Output: 99
